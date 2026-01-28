81d161dcedf3d0b473711ce57fb5dcb182e35441
Ben53278 <143300029+bgaveev@users.noreply.github.com>
2026-01-28 18:24:39 -0500

    Created Shooter.h
    Added shooter namespace with timeout constants, speed constants, and velocity setpoints
    Added Shooter class

---
+++ src/main/include/Shooter.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "RobotIO.h"
+#include <frc/Timer.h>
+
+namespace shooter
+{
+    enum eState
+    {
+        STATE_START = 0,
+        STATE_IDLE = 1,
+        STATE_LOW_POWER_RAMP_UP = 2,
+        STATE_HIGH_POWER_RAMP_UP = 3,
+        STATE_SHOOT = 4,
+        STATE_ERROR = 99
+    };
+
+    enum eCommand 
+    {
+        COMMAND_NONE,
+        COMMAND_LOW_POWER_SHOOT,
+        COMMAND_HIGH_POWER_SHOOT,
+        COMMAND_STOP
+    };
+
+    // Timeout Constants
+    static constexpr double dRampUpTimeout = 4.0;
+
+    // Motor Speed Constants
+    static constexpr double dLowPowerRampUpSpeed = 0.3;
+    static constexpr double dHighPowerRampUpSpeed = 0.55;
+    static constexpr double dFeederSpeed = 0.2;
+
+    // Shooter Velocity Setpoints
+    static constexpr double dLowPowerVelocitySetpoint = 50.0;
+    static constexpr double dHighPowerVelocitySetpoint = 100.0;
+}
+
+
+class Shooter
+{
+public:
+    // Constructor/ Deconstructor
+    Shooter();
+    ~Shooter()
+        {   }
+    
+    // Accessor Methods
+    inline bool isIdle()
+        { return ( m_eState == shooter::eState::STATE_IDLE ); }
+    inline bool isRampingUp()
+        { return ( m_eState == shooter::eState::STATE_HIGH_POWER_RAMP_UP || m_eState == shooter::eState::STATE_LOW_POWER_RAMP_UP ); }
+    inline bool isShooting()
+        { return ( m_eState == shooter::eState::STATE_SHOOT ); }
+
+    inline void LowPowerShoot()
+        { m_eCommand = shooter::eCommand::COMMAND_LOW_POWER_SHOOT; }
+    inline void HighPowerShoot()
+        { m_eCommand = shooter::eCommand::COMMAND_HIGH_POWER_SHOOT; }
+    inline void Stop()
+        { m_eCommand = shooter::eCommand::COMMAND_STOP; }
+
+    // Class Methods
+    void Initialize( RobotIO *p_pRobotIO );
+    void Execute();
+
+private:
+
+    shooter::eState m_eState;
+    shooter::eCommand m_eCommand;
+
+    frc::Timer *m_pTimeoutTimer;
+
+    RobotIO *m_pRobotIO;
+
+    // Add Motor Configs here
+
+};
