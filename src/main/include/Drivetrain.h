e4dd707569451782006166123a69a53f3143d9b6
Spatonn <94061852+Spatonn@users.noreply.github.com>
2026-04-02 20:01:37 -0400

    Revert "Speed Change; Increase 4 to 4.5"
    This reverts commit 84764b4c57f83a1218f4a64f238b7b18eb386fc9.

--- src/main/include/Drivetrain.h
+++ src/main/include/Drivetrain.h
@@ -38,7 +38,7 @@ namespace drivetrain
 {
     // Constant Values for max bot linear velocity (Drive Speed) and angular velocity (Spin/turn Speed)
 
-    static constexpr units::meters_per_second_t kMaxSpeed = 4.5_mps;  // 2.5 meters per second
+    static constexpr units::meters_per_second_t kMaxSpeed = 4_mps;  // 2.5 meters per second
     static constexpr units::radians_per_second_t kMaxAngularSpeed{ std::numbers::pi };  // 1/2 rotation per second 
 
     static constexpr units::meter_t kWheelDistance = 0.54782769475136_m;  // The distance bewteen 2 adjacent (like 1 edge of the square, not the diagnol) 
